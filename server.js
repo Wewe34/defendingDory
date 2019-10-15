7f2ae05acbbc40c0a58b21a9e495dbaca3a9fb5f
Cassandra Terry <cassandra34terry@gmail.com>
2019-10-14 23:46:24 -0500

    package.json script start

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 const path = require('path');
 
 
-app.use('/public', express.static(path.join(__dirname, 'public')), app.use(express.static('scenes')))
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.listen(process.env.PORT || 1341, function(){
 	console.log('Listening on Port 8888..');
   });
