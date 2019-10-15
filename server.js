57148a972dfe360184e6e8e332f343f185722e3a
Cassandra Terry <cassandra34terry@gmail.com>
2019-10-15 17:11:52 -0500

    sever added

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, '/src')));
+app.listen(process.env.PORT || 1341, function(){
+	console.log('Listening on Port 1341..');
+  });
